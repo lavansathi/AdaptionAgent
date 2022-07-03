95055b909c85773d310ab4933496aa822b8ea9c7
nico8034 <56026876+nico8034@users.noreply.github.com>
2022-07-03 19:17:13 +0200

    implementation of javascript q-learning

--- JavaScript_implementation/src/index.ts
+++ JavaScript_implementation/src/index.ts
@@ -2,19 +2,34 @@ import internal from "stream";
 import { EmotionalState } from "./classes/EmotionalState";
 import { UIElement } from "./classes/UIElement";
 
+function createNDimArray(dimensions:any) {
+    if (dimensions.length > 0) {
+        var dim = dimensions[0];
+        var rest = dimensions.slice(1);
+        var newArray = new Array();
+        for (var i = 0; i < dim; i++) {
+            newArray[i] = createNDimArray(rest);
+        }
+        return newArray;
+     } else {
+        return 0;
+     }
+ }
+
+
 class Agent {
     private emotionalState:EmotionalState = new EmotionalState(0);
     private UIElements:UIElement[] = [];
     private gamma = 0.9;
     private alpha = 0.9;
     private epsilon = 0.01;
-    private q_values = {};
-    private rewards = {}
+    private q_values:any[] = [];
+    private rewards:any[] = [];
     private actions:string[] = []
 
     constructor(){
     }
-
+    
     addUIElement = (uiElement:UIElement) => {
         this.UIElements.push(uiElement)
     }
@@ -45,11 +60,19 @@ class Agent {
     getActions = () => {
         return this.actions;
     }
-    is_terminal_state = (UIElements:UIElement[],EmotionalState:EmotionalState) => {
-        // if rewards == 1 for state 
-        //  return True
-    //      else
-        //  return False
+    
+    is_terminal_state = () => {
+        let e1 = this.getUIElement(0).getProperty();
+        let e2 = this.getUIElement(1).getProperty();
+        let e3 = this.getUIElement(2).getProperty();
+        let e4 = this.getUIElement(3).getProperty();
+        let emotional_state = this.getEmotionalState().getCurrentEmotion();
+        if (this.rewards[0][e1][e2][e3][e4][emotional_state] == 1){
+            return true
+        } else {
+            return false
+        }
+    
     }
     get_starting_location = () => {
         // choose a random non-terminal state for the elements
@@ -80,7 +103,7 @@ class Agent {
         let minSum = 0;
         let maxSum = this.getUIElements().length*2;
 
-        console.log(maxSum)
+        //console.log(maxSum)
 
         let e1 = this.getUIElement(0).getProperty();
         let e2 = this.getUIElement(1).getProperty();
@@ -88,7 +111,7 @@ class Agent {
         let e4 = this.getUIElement(3).getProperty();
 
         let currentSum = (e1+e2+e3+e4);
-        console.log(currentSum)
+        //console.log(currentSum)
 
         // lower 25%
         if (currentSum < Math.floor(0.25*maxSum)){
@@ -105,7 +128,7 @@ class Agent {
 
     }
 
-    get_next_action = () => {
+    get_next_action = ():number => {
 
         if(Math.random() < this.epsilon){
             // return np.argmax(q_values[current_element1_property, current_element2_property, current_element3_property, current_emontionalState])
@@ -115,32 +138,112 @@ class Agent {
         }
     }
 
-    get_next_state = () => {
+    get_next_state = (action_index:number) => {
+        let e1 = this.getUIElement(0);
+        let e2 = this.getUIElement(1);
+        let e3 = this.getUIElement(2);
+        let e4 = this.getUIElement(3);
         
+        // Element 1 actions
+        if(this.actions[action_index] == 'element1_action0' && e1.getProperty() != 0){
+            e1.setProperty(0)
+        } else if(this.actions[action_index] == 'element1_action1' && e1.getProperty() != 1){
+            e1.setProperty(1)
+        } else if(this.actions[action_index] == 'element1_action2' && e1.getProperty() != 2){
+            e1.setProperty(2)
+        } else if(this.actions[action_index] == 'element2_action0' && e2.getProperty() != 0){
+            e2.setProperty(0)
+        } else if(this.actions[action_index] == 'element2_action1' && e2.getProperty() != 1){
+            e2.setProperty(1)
+        } else if(this.actions[action_index] == 'element2_action2' && e2.getProperty() != 2){
+            e2.setProperty(2)
+        } else if(this.actions[action_index] == 'element3_action0' && e3.getProperty() != 0){
+            e3.setProperty(0)
+        } else if(this.actions[action_index] == 'element3_action1' && e3.getProperty() != 1){
+            e3.setProperty(1)
+        } else if(this.actions[action_index] == 'element3_action2' && e3.getProperty() != 2){
+            e3.setProperty(2)
+        } else if(this.actions[action_index] == 'element4_action0' && e4.getProperty() != 0){
+            e4.setProperty(0)
+        } else if(this.actions[action_index] == 'element4_action1' && e4.getProperty() != 1){
+            e4.setProperty(1)
+        } else if(this.actions[action_index] == 'element4_action2' && e4.getProperty() != 2){
+            e4.setProperty(2)
+        } else {
+            this.get_next_state(this.get_next_action())
+        }
+
+        // Update emotional state
+        this.update_user_emotion();
+
     }
 
 
     setupAgent = () => {
 
-        // Generate Actions based on properties on elements
-        console.log(this.getUIElements())
-
+        // Generate Actions based on elements and property count
         let actionsArr = []
         for (let i = 0; i < this.getUIElements().length; i++) {
             for (let j = 0; j < this.getUIElement(i).getPropertyCount(); j++) {
-                actionsArr.push('element'+(i+1)+'_action'+(j+1))
+                actionsArr.push('element'+(i+1)+'_action'+(j))
             }
         }
         this.actions = actionsArr;
+        // Initialize Q-table with 0 values for all pairs
+        let QtableArr = createNDimArray([
+            this.getUIElement(0).getPropertyCount(),
+            this.getUIElement(1).getPropertyCount(),
+            this.getUIElement(2).getPropertyCount(),
+            this.getUIElement(3).getPropertyCount(),
+            this.getEmotionalState().getAvailableStateCount(),
+            this.getActions().length
+        ])
 
-        console.log(this.actions[this.get_next_action()])
-        console.log(
-            this.getUIElement(0).getProperty(),
-            this.getUIElement(1).getProperty(),
-            this.getUIElement(2).getProperty(),
-            this.getUIElement(3).getProperty(),
-            this.getEmotionalState().getCurrentEmotion()
-        )
+        this.q_values.push(QtableArr)
+
+        //Initialize rewards table
+        let rewardArr = createNDimArray([
+            this.getUIElement(0).getPropertyCount(),
+            this.getUIElement(1).getPropertyCount(),
+            this.getUIElement(2).getPropertyCount(),
+            this.getUIElement(3).getPropertyCount(),
+            this.getEmotionalState().getAvailableStateCount(),
+        ])
+        this.rewards.push(rewardArr)
+
+        let rewardArray = this.rewards[0]
+        // [Element1][Element2][Element3][Element4][Emotional_State]
+        console.log(this.getUIElement(0).getPropertyCount())
+        console.log(this.getUIElement(1).getPropertyCount())
+        console.log(this.getUIElement(2).getPropertyCount())
+        console.log(this.getUIElement(3).getPropertyCount())
+
+        for (let m = 0; m < this.getUIElement(0).getPropertyCount(); m++) {
+            for (let n = 0; n < this.getUIElement(1).getPropertyCount(); n++) {
+                for (let o = 0; o < this.getUIElement(2).getPropertyCount(); o++) {
+                    for (let p = 0; p < this.getUIElement(3).getPropertyCount(); p++) {
+                        // Negative rewards
+                        rewardArray[m][n][o][p][4] = -1;
+                        rewardArray[m][n][o][p][5] = -2;
+                        rewardArray[m][n][o][p][6] = -3;
+                    }
+                }
+            }
+        }
+
+        // Positive rewards
+        rewardArray[1][1][1][1][0] = 1;
+        rewardArray[1][1][1][1][1] = 1;
+        rewardArray[1][1][1][1][2] = 1;
+
+        console.log(rewardArray[1][1][1][1][5])
+
+
+
+
+        let nextAction = this.get_next_action();
+
+        console.log(this.actions[nextAction])
 
         this.get_starting_location()
 
@@ -151,8 +254,8 @@ class Agent {
             this.getUIElement(3).getProperty(),
             this.getEmotionalState().getCurrentEmotion()
         )
-
-        this.update_user_emotion()
+        
+        this.get_next_state(nextAction);
 
         console.log(
             this.getUIElement(0).getProperty(),
@@ -165,8 +268,24 @@ class Agent {
     }
 
     init = () => {
-       
+        
+        /*
+        for (let i = 0; i < this.q_values[0].length; i++) {
+            for (let j = 0; j < this.q_values[0][0].length; j++) {
+                for (let k = 0; k < this.q_values[0][0][0].length; k++) {
+                    for (let l = 0; l < this.q_values[0][0][0][0].length; l++) {
+                        for (let h = 0; h < this.q_values[0][0][0][0][0].length; h++) {
+                        //console.log(i,j,k,l,h)
+                            //this.q_values[0][0][0][0][0] = 0;
+                        }
+                    }
+                }
+            }
+        }
 
+        console.log(this.q_values[0][0][0])
+        console.log(this.q_values[0][0][0][0][0])
+        */
     }
 }
 
@@ -179,4 +298,4 @@ agent.addUIElement(new UIElement(3,2,'element4'))
 
 
 agent.setupAgent()
-
+agent.init()
