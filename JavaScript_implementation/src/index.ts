483bb291cbf01fc0798b3bb3ee2479109690712c
nico8034 <56026876+nico8034@users.noreply.github.com>
2022-07-05 12:46:51 +0200

    Cleanup in code

--- JavaScript_implementation/src/index.ts
+++ JavaScript_implementation/src/index.ts
@@ -390,31 +390,26 @@ class Agent {
         // Update Q-value for the previous state and action pair
         let new_q_value = old_q_value + (this.getAplha() * temporal_difference)
         this.getQ_Values()[old_e1][old_e2][old_e3][old_e4][old_emotion][action_index] = new_q_value
-        //console.log('new_q_value: '+new_q_value)
       }
     }
     console.log('Training Complete!')
-    console.log(training_range+' rounds')
   }
 
 }
 
 const agent = new Agent(0.9,0.9,0.9)
 
-
 agent.setupAgent()
 agent.train(10000)
+
 let adaptionResult = agent.get_best_adaption(0,0,0,0,4)
 console.log(adaptionResult)
 console.log('Number of adaptions to goal '+adaptionResult.length)
+
 let adaptionResult3 = agent.get_best_adaption(2,2,2,2,5)
 console.log(adaptionResult3)
 console.log('Number of adaptions to goal '+adaptionResult3.length)
+
 let adaptionResult4 = agent.get_best_adaption(2,0,2,0,6)
 console.log(adaptionResult4)
 console.log('Number of adaptions to goal '+adaptionResult4.length)
-
-
-
-
-//agent.init()
