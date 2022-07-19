0f1646c6ffe608b4f875f683fbefb3220f225b2d
nico8034 <56026876+nico8034@users.noreply.github.com>
2022-07-19 05:17:35 +0200

    Remove terminal condition, make point based

--- src/classes/Agent.ts
+++ src/classes/Agent.ts
@@ -10,17 +10,18 @@ export class Agent {
   private q_values:any[] = [];
   private rewards:any[] = [];
   private actions:string[] = []
+  private rewardSum:number = 0;
 
   constructor(alpha:number,gamma:number,epsilon:number){  
     this.alpha = alpha;
     this.gamma = gamma;
     this.epsilon = epsilon;
 
-    this.UIElements.push(new UIElement(3,2,'element1'))
-    this.UIElements.push(new UIElement(3,2,'element2'))
-    this.UIElements.push(new UIElement(3,2,'element3'))
-    this.UIElements.push(new UIElement(3,2,'element4'))
-    this.UIElements.push(new UIElement(3,2,'element5'))
+    this.UIElements.push(new UIElement(2,0,'element1'))
+    this.UIElements.push(new UIElement(2,0,'element2'))
+    this.UIElements.push(new UIElement(2,0,'element3'))
+    this.UIElements.push(new UIElement(2,0,'element4'))
+    this.UIElements.push(new UIElement(2,0,'element5'))
 
   }
     
@@ -54,18 +55,16 @@ export class Agent {
   getEpsilon = () => {
     return this.epsilon;
   }
-  setRewards = (rewardsArr:any[]) => {
+  setRewardsTable = (rewardsArr:any[]) => {
     this.rewards = rewardsArr;
   }
-  getRewards = () => {
-    // returning content of rewards without "outermost" array
+  getRewardsTable = () => {
     return this.rewards;
   }
   setQ_Values = (qtable:any[]) => {
     this.q_values = qtable;
   }
   getQ_Values = () => {
-    // returning content of q-table without "outermost" array
     return this.q_values;
   }
   setActions = (actions:string[]) => {
@@ -73,6 +72,12 @@ export class Agent {
   }
   getActions = () => {
     return this.actions;
+  }
+  setRewardSum = (reward:number) => {
+    this.rewardSum += reward;
+  }
+  getRewardSum = () => {
+    return this.rewardSum;
   }
 
   is_terminal_state = () => {
@@ -82,7 +87,9 @@ export class Agent {
     let e4 = this.getUIElement(3).getState();
     let e5 = this.getUIElement(4).getState();
     let emotional_state = this.getEmotionalState().getCurrentEmotion();
-    if (this.getRewards()[e1][e2][e3][e4][e5][emotional_state] === 1){
+
+    if(this.getRewardSum() == 1000){
+    //if (this.getRewardsTable()[e1][e2][e3][e4][e5][emotional_state] === 1){
     // if (this.getEmotionalState().getCurrentEmotion() === 0 || this.getEmotionalState().getCurrentEmotion() === 2){
       return true
     } else {
@@ -181,7 +188,7 @@ export class Agent {
       let e2 = this.getUIElement(1).getState();
       let e3 = this.getUIElement(2).getState();
       let e4 = this.getUIElement(3).getState();
-      let e5 = this.getUIElement(3).getState();
+      let e5 = this.getUIElement(4).getState();
 
       let emotion = this.getEmotionalState().getCurrentEmotion();
 
@@ -208,32 +215,22 @@ export class Agent {
       e1.setState(0)
     } else if(this.actions[action_index] == 'element1_action1' && e1.getState() != 1){
       e1.setState(1)
-    } else if(this.actions[action_index] == 'element1_action2' && e1.getState() != 2){
-      e1.setState(2)
     } else if(this.actions[action_index] == 'element2_action0' && e2.getState() != 0){
       e2.setState(0)
     } else if(this.actions[action_index] == 'element2_action1' && e2.getState() != 1){
       e2.setState(1)
-    } else if(this.actions[action_index] == 'element2_action2' && e2.getState() != 2){
-      e2.setState(2)
     } else if(this.actions[action_index] == 'element3_action0' && e3.getState() != 0){
       e3.setState(0)
     } else if(this.actions[action_index] == 'element3_action1' && e3.getState() != 1){
       e3.setState(1)
-    } else if(this.actions[action_index] == 'element3_action2' && e3.getState() != 2){
-      e3.setState(2)
     } else if(this.actions[action_index] == 'element4_action0' && e4.getState() != 0){
       e4.setState(0)
     } else if(this.actions[action_index] == 'element4_action1' && e4.getState() != 1){
       e4.setState(1)
-    } else if(this.actions[action_index] == 'element4_action2' && e4.getState() != 2){
-      e4.setState(2)
     } else if(this.actions[action_index] == 'element5_action0' && e5.getState() != 0){
       e5.setState(0)
     } else if(this.actions[action_index] == 'element5_action1' && e5.getState() != 1){
       e5.setState(1)
-    } else if(this.actions[action_index] == 'element5_action2' && e5.getState() != 2){
-      e5.setState(2)
     } else {
       // not 100% sure if this is needed but uncase agent want to take action resulting in its current state
       this.get_next_state(this.get_next_action(this.getEpsilon()))
@@ -296,24 +293,35 @@ export class Agent {
       this.getEmotionalState().getAvailableState_Count(),
   ])
   //@ts-ignore
-  this.setRewards(rewardArr)
+  this.setRewardsTable(rewardArr)
 
-    let rewardArray = this.getRewards()
+    let rewardArray = this.getRewardsTable()
     // [Element1][Element2][Element3][Element4][Element5][Emotional_State]
     for (let m = 0; m < this.getUIElement(0).getState_Count(); m++) {
       for (let n = 0; n < this.getUIElement(1).getState_Count(); n++) {
         for (let o = 0; o < this.getUIElement(2).getState_Count(); o++) {
           for (let p = 0; p < this.getUIElement(3).getState_Count(); p++) {
             for (let q = 0; q < this.getUIElement(3).getState_Count(); q++) {
+              
+              // Happy
+              rewardArray[m][n][o][p][q][0] = 5;
+              // Surprised
+              rewardArray[m][n][o][p][q][2] = 2;
               // Negative rewards
               rewardArray[m][n][o][p][q][4] = -1;
               rewardArray[m][n][o][p][q][5] = -3;
               rewardArray[m][n][o][p][q][6] = -6;
 
-              // TESTING THIS
-              // Positive rewards
-              rewardArray[m][n][o][p][q][0] = 1;
-              rewardArray[m][n][o][p][q][2] = 1;
+              /*  Emotional States
+                0 - Happiness (+5)
+                1 - Sadness (0)
+                2 - Surprise (+2)
+                3 - Fear (0)
+                4 - Anger (-1)
+                5 - Disgust (-3)
+                6 - Contempt (-6)
+            */
+
             }
           }
         }
@@ -322,6 +330,7 @@ export class Agent {
   }
 
   populateQtable = () => {
+    //[FontSize][ColorTheme][Destination][FontType][DialogVisibility][Emotion][Action]
     // Setting initial values for the Q-table
     this.getQ_Values()[0][0][0][0][0][4][1] = 1
     this.getQ_Values()[1][0][0][0][0][4][3] = 1
@@ -396,13 +405,21 @@ export class Agent {
 
         // Receive reward for moving to new state
         // Calculate temporal_difference
-        let reward = this.getRewards()[this.getUIElement(0).getState()][this.getUIElement(1).getState()][this.getUIElement(2).getState()][this.getUIElement(3).getState()][this.getUIElement(4).getState()][this.getEmotionalState().getCurrentEmotion()]
+        let reward = this.getRewardsTable()[this.getUIElement(0).getState()][this.getUIElement(1).getState()][this.getUIElement(2).getState()][this.getUIElement(3).getState()][this.getUIElement(4).getState()][this.getEmotionalState().getCurrentEmotion()]
+        this.setRewardSum(reward)
         let old_q_value = this.getQ_Values()[old_e1][old_e2][old_e3][old_e4][old_e5][old_emotion][action_index]
         let temporal_difference = reward + (this.getGamma() * Math.max(...this.getQ_Values()[this.getUIElement(0).getState()][this.getUIElement(1).getState()][this.getUIElement(2).getState()][this.getUIElement(3).getState()][this.getUIElement(4).getState()][this.getEmotionalState().getCurrentEmotion()])) - old_q_value
         
         // Update Q-value for the previous state and action pair
         let new_q_value = old_q_value + (this.getAplha() * temporal_difference)
         this.getQ_Values()[old_e1][old_e2][old_e3][old_e4][old_e5][old_emotion][action_index] = new_q_value
+        console.log(this.getRewardSum())
+        console.log(this.getUIElement(0).getState(),
+                      this.getUIElement(1).getState(),
+                      this.getUIElement(2).getState(),
+                      this.getUIElement(3).getState(),
+                      this.getUIElement(4).getState(),
+                      this.getEmotionalState().getCurrentEmotion())
       }
     }
     console.log('Training Complete!')
@@ -440,14 +457,16 @@ export class Agent {
   
         // Receive reward for moving to new state
         // Calculate temporal_difference
-        let reward = this.getRewards()[this.getUIElement(0).getState()][this.getUIElement(1).getState()][this.getUIElement(2).getState()][this.getUIElement(3).getState()][this.getUIElement(4).getState()][this.getEmotionalState().getCurrentEmotion()]
+        let reward = this.getRewardsTable()[this.getUIElement(0).getState()][this.getUIElement(1).getState()][this.getUIElement(2).getState()][this.getUIElement(3).getState()][this.getUIElement(4).getState()][this.getEmotionalState().getCurrentEmotion()]
+        this.setRewardSum(reward)
         let old_q_value = this.getQ_Values()[old_e1][old_e2][old_e3][old_e4][old_e5][old_emotion][action_index]
         let temporal_difference = reward + (this.getGamma() * Math.max(...this.getQ_Values()[this.getUIElement(0).getState()][this.getUIElement(1).getState()][this.getUIElement(2).getState()][this.getUIElement(3).getState()][this.getUIElement(4).getState()][this.getEmotionalState().getCurrentEmotion()])) - old_q_value
         
         // Update Q-value for the previous state and action pair
         let new_q_value = old_q_value + (this.getAplha() * temporal_difference)
         this.getQ_Values()[old_e1][old_e2][old_e3][old_e4][old_e5][old_emotion][action_index] = new_q_value
-          console.log(this.getUIElement(0).getState(),
+        console.log(this.getRewardSum())
+        console.log(this.getUIElement(0).getState(),
                       this.getUIElement(1).getState(),
                       this.getUIElement(2).getState(),
                       this.getUIElement(3).getState(),
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ const agent = new Agent(0.9,0.9,0.9)
 
 agent.setupAgent()
 //agent.run(0,0,0,0,0,4)
-agent.train(100000)
+agent.train(1000)
 agent.run(0,0,0,0,0,4)
 
-//let adaptionResult = agent.get_best_adaption(0,0,0,0,0,4)
-//console.log(adaptionResult)
-//console.log('Number of adaptions to goal '+adaptionResult.length)
+/*
+let adaptionResult = agent.get_best_adaption(0,0,0,0,0,4)
+console.log(adaptionResult)
+console.log('Number of adaptions to goal '+adaptionResult.length)
+console.log("Goal "+adaptionResult[adaptionResult.length-1])
+*/
